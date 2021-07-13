754941db07ccd6b02946bbaa50774fd420c1bff3
VP <VP>
2021-07-13 23:15:28 +0300

    commit #1

--- ScanningDocumentApp/ViewController.swift
+++ ScanningDocumentApp/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import VisionKit
 
 class ViewController: UIViewController {
 
@@ -14,6 +15,27 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchUpInsideCameraButton(_ sender: Any) {
+        
+        configureDocumentView()
+    }
+    
+    private func configureDocumentView() {
+        let scanningDocumentVC = VNDocumentCameraViewController()
+        scanningDocumentVC.delegate = self
+        self.present(scanningDocumentVC, animated: true, completion: nil)
+    }
 }
 
+extension ViewController: VNDocumentCameraViewControllerDelegate {
+    
+    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+        
+        for pageNumber in 0..<scan.pageCount {
+            let image = scan.imageOfPage(at: pageNumber)
+            print(image)
+        }
+        controller.dismiss(animated: true, completion: nil)
+        
+    }
+}
